6aaa2689052419b2e6f889fbedbfc3f971f89ec2
kareemshurafa <kareemshurafa@gmail.com>
2025-11-20 20:53:02 +0000

    Create basic app.py with initialisation of env variables

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import boto3
+import os
+
+app = Flask(__name__)
+
+# Reference - https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.environ['S3_KEY'], # access key for AWS account
+    aws_secret_access_key=os.environ['S3_ACCESS'] # secret key for AWS account
+)
+
+@app.route("/")
+def home():
+    return "<p> URLGetter is live! </p>"
