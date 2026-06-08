ab65fe764b189b26defc6b8b98b7f51ab1b880fb
kareemshurafa <kareemshurafa@gmail.com>
2026-06-08 23:56:18 +0100

    bug fix for connecting to Redis for background worker

--- worker.py
+++ worker.py
@@ -2,6 +2,7 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 local = False
 if local:
@@ -12,6 +13,10 @@ listen = ['high', 'default', 'low']
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
+
+# Reference - https://devcenter.heroku.com/articles/connecting-heroku-redis#connecting-in-python
+url = urlparse(os.environ.get("REDIS_URL"))
+conn = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
 
 if __name__ == '__main__':
     with Connection(conn):
